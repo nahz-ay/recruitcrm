8d22ff548f765ca36c070ac3586034baa046304c
nahz-ay <nahasay5@gmail.com>
2024-05-26 17:03:12 +0530

    icons package updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 // import styles from "./page.module.css";
+import "boxicons/css/boxicons.min.css";
+
 import SideNav from "@/components/SideNav";
+import Head from "next/head";
 
 export default function Home() {
+  
   return (
     <>
-    <SideNav/>
+      <SideNav />
     </>
   );
 }
